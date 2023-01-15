6c9183723f415a2849643181d6f523d5ba88ca50
Michal Miškerník <michal@miskernik.sk>
2023-01-15 09:57:49 +0100

    Fixed background overflow in list selector

--- components/list-selector/list-selector.tsx
+++ components/list-selector/list-selector.tsx
@@ -7,7 +7,7 @@ export function ListSelector() {
 
   return (
     <div className="flex flex-col gap-4">
-      <ul className="border border-slate-400 rounded divide-y divide-slate-400">
+      <ul className="border border-slate-400 rounded divide-y divide-slate-400 overflow-hidden">
         {lists?.map((list) => (
           <li key={list.id}>
             <ListLink listId={list.id} color={list.color} title={list.title} />
